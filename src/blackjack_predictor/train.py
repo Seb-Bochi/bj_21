8661e7221da50a9ff66ed0b93c9d4a99fa922ad8
AIAndreas <a.rosenquisten@gmail.com>
2026-06-12 11:02:10 +0200

    .

---
+++ src/blackjack_predictor/train.py
@@ -0,0 +1,10 @@
+from blackjack_predictor.model import Model
+from blackjack_predictor.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
