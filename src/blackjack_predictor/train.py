2f043bc1437c1af59d2b59a07275f4a81b1a2c74
Sebastian Bochineck <sebbochineck@gmail.com>
2026-06-12 11:54:41 +0200

    train?

--- src/blackjack_predictor/train.py
+++ src/blackjack_predictor/train.py
@@ -1,10 +1,30 @@
 from blackjack_predictor.model import Model
 from blackjack_predictor.data import MyDataset
+from torch import nn, optim
 
 def train():
     dataset = MyDataset("data/raw")
     model = Model()
     # add rest of your training code here
+    criterion = nn.NLLLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.003)
+
+    epochs = 5
+    for _ in range(epochs):
+        running_loss = 0
+        
+        for states, actions in trainloader:
+            predictions = model(states)
+
+            loss = criterion(predictions, actions)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            runnig_loss += loss.item()
+        else:
+            print("fTraining loss: {runnig_loss / len(trainloader)}")
 
 if __name__ == "__main__":
     train()
