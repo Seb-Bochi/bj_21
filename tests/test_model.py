b22066715979ed0e07dfecfd95affcea62344376
nojayz <jason933julienne@gmail.com>
2026-06-18 11:20:48 +0200

    modifying the test_model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,11 @@
 import torch
 
-from blackjack_predictor.model import Model
+from blackjack_predictor.models.ffnn import SimpleFNN
 
 
 def test_model_forward_shape():
 	"""Test that the model returns a two-class output."""
-	model = Model()
+	model = SimpleFNN(input_dim=13, hidden_dim=64, output_dim=2)
 	output = model(torch.rand(4, 13))
 
 	assert output.shape == (4, 2)
