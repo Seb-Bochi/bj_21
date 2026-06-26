22b9778cc7e6e1d730143b0ed2d1448cda6428be
Sebastian Bochineck <sebbochineck@gmail.com>
2026-06-26 10:45:25 +0200

    vertex display name fixed

--- scripts/submit_vertex_training.sh
+++ scripts/submit_vertex_training.sh
@@ -5,6 +5,7 @@ PROJECT_ID="${PROJECT_ID:-dtumlops-499809}"
 REGION="${REGION:-europe-west1}"
 IMAGE_URI="${IMAGE_URI:-europe-west1-docker.pkg.dev/${PROJECT_ID}/dtumlops/blackjack-train:latest}"
 JOB_SPEC="${JOB_SPEC:-infra/vertex/blackjack_train_custom_job.yaml}"
+JOB_DISPLAY_NAME="${JOB_DISPLAY_NAME:-blackjack-train}"
 
 gcloud builds submit \
   --project "${PROJECT_ID}" \
@@ -14,4 +15,5 @@ gcloud builds submit \
 gcloud ai custom-jobs create \
   --project "${PROJECT_ID}" \
   --region "${REGION}" \
+  --display-name "${JOB_DISPLAY_NAME}" \
   --config "${JOB_SPEC}"
